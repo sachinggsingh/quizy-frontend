e538a13e33869c750ffd0765bb5421b940c73656
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2026-05-10 15:52:19 +0530

    fix: backend url port fixed

--- lib/api/subscription.ts
+++ lib/api/subscription.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3003";
 
 export const getSubscription = async () => {
   const res = await fetch(`${BACKEND_URL}/subscription`, {
